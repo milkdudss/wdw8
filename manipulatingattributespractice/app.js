86221c1387a05798c61d4021fe0ec82cbfb2fbcb
milkdudss <albertkoshy@gmail.com>
2020-10-30 12:25:14 -0700

    w

---
+++ manipulatingattributespractice/app.js
@@ -0,0 +1,4 @@
+// The url you need: 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg'
+let egg = document.getElementById('egg') ;
+egg.setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg');
+egg.setAttribute('alt', 'chicken');
